e4c554281593ed415ff98ed13053b29c7667183a
Alemberht <104173691+Alemberht@users.noreply.github.com>
2024-10-19 21:11:37 -0300

    Add files via upload

--- Textos.js
+++ Textos.js
@@ -2,17 +2,17 @@ function textos (){
 texto[1]= ("Llega a mordor con frodo y sam. quemando el anillo único");
 texto[2]= ("Al salir de la comarca, te encuentras con unos hobbits \n llamados merry y pippin");
 texto[3]= ("Caen a un barranco y se encuentran a un nazgúl. Qué hacen? ''");
-texto[4]= ("Pasan desapercibidos, logrando huír hacia un pueblo llamado bree, pidiendo alojamiento");
-texto[7]= ("conocen a un montaraz que se hace llamar trancos, ahora los acompaña en su viaje");
-texto[22]= ("llegan a rivendell y se enteran que tienen que llevar el anillo a mordor al monte del destino, destruyéndolo en el acto");
+texto[4]= ("Pasan desapercibidos, logrando huír hacia un pueblo \n llamado bree, pidiendo alojamiento");
+texto[7]= ("conocen a un montaraz que se hace llamar trancos, \n ahora los acompaña en su viaje");
+texto[22]= ("llegan a rivendell y se enteran que tienen que llevar \n el anillo a mordor al monte del destino,  \ndestruyéndolo en el acto");
 texto[8]= ("Salen de Rivendel, acompañados por 4 compañeros más, gandalf, Boromir, Legolas y Gimli");
-texto[9]= ("Llegan a Minas moria, allí gandalf muere y logran pasar al otro lado, llegando al paso de gondor");
+texto[9]= ("Llegan a Minas moria, allí gandalf muere y logran pasar \n al otro lado, llegando al paso de gondor");
 texto[10]= ("Luego Boromir muere intentado quitartle el anillo a frodo");
-texto[11]= ("Sam te decide acompañar en tu viaje solo y emprender el viaje a mordor junto a ti");
+texto[11]= ("Sam te decide acompañar en tu viaje solo y emprender el \n viaje a mordor junto a ti");
 texto[12]= (" Conocen a una criatura llamada Gollum y los acompaña en el viaje");
 texto[21]= ("Logran llegar a mordor");
-texto[13]= ("Al llegar al monte del destino el anillo te tienta, ofreciéndote juventud y poder");
-texto[18]= ("Corrupto, te pones el anillo. viniendo gollum hacia ti. Arráncandote el dedo del anillo, en eso, los dos se caen con gollum teniendo el anillo hacia la boca del volcána.");
+texto[13]= ("Al llegar al monte del destino el anillo te tienta, \n ofreciéndote juventud y poder");
+texto[18]= ("Corrupto, te pones el anillo. viniendo gollum hacia ti.  \nArráncandote el dedo del anillo, en eso, los dos se \n caen con gollum teniendo el anillo hacia la boca del volcána.");
 texto[19]= ("Sam te salva de morir");
 texto[20]= ("La tierra media se ha salvado");
 
@@ -22,17 +22,17 @@ texto[25]= ("reiniciar");
 //ruta secndaria 1
 
 texto[5]= ("Los nazgúl los persiguen y les dan caza");
-texto[6]= ("El anillo único ahora es de Sauron, la tierra media ahora no tiene futuro más que la eterna oscuridad");
+texto[6]= ("El anillo único ahora es de Sauron, la tierra media ahora \n no tiene futuro más que la eterna oscuridad");
 
 //ruta secndaria 2
 
-texto[14]= ("Con el tiempo , alguien más intenta robarte el anillo en medio de la noche");
+texto[14]= ("Con el tiempo , alguien más intenta robarte el \n anillo en medio de la noche");
 texto[15]= ("Te lo roba, y desaparece. haciendo que el anillo se pierda por milenios una vez más");
 
 //ruta secndaria 3
 
-texto[16]= ("Al intentarlo arrojar al volcán por voluntad propia, gollum te mata a traición, robándo el anillo en el acto");
-texto[17]= ("Ahora el anillo lo tiene gollum y  es cuestión de tiempo para que los Nazgul lo recuperen");
+texto[16]= ("Al intentarlo arrojar al volcán por voluntad propia, \n gollum te mata a traición, robándo el anillo en el acto");
+texto[17]= ("Ahora el anillo lo tiene gollum y  es cuestión de \n tiempo para que los Nazgul lo recuperen");
 
 //DECISIONES
 decision[1] = ("Esconderse");
